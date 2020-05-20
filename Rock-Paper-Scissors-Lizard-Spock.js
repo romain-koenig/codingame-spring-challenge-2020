f05e2451ed81dd0a092cb9b92c9a767f4a30cbf8
Romain KOENIG <romain.koenig@gmail.com>
2020-05-20 16:08:26 +0200

    Init // default code

---
+++ Rock-Paper-Scissors-Lizard-Spock.js
@@ -0,0 +1,16 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N = parseInt(readline());
+for (let i = 0; i < N; i++) {
+    var inputs = readline().split(' ');
+    const NUMPLAYER = parseInt(inputs[0]);
+    const SIGNPLAYER = inputs[1];
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('WHO IS THE WINNER?');
